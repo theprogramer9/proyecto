d5103d9d382499edfa3e901d14aa4b18ba6ffc33
theprogramer9 <38675543+theprogramer9@users.noreply.github.com>
2018-08-16 14:04:54 -0500

    menu
    menu de infromacion

--- system/menu.php
+++ system/menu.php
@@ -7,7 +7,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Administracion Floreria</a>
+            <a class="navbar-brand" href="#">Administracion CarnesFrias</a>
           </div>
           <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
